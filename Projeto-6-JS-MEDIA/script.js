8d03fb3272622bf7215aafb3f615284a784d6c4b
CELSO PEREIRA LIMA JUNIOR <celso.pljunior@senacsp.edu.br>
2025-04-30 17:26:22 -0300

    Conversor 3 + Tabuada

--- Projeto-6-JS-MEDIA/script.js
+++ Projeto-6-JS-MEDIA/script.js
@@ -1,3 +1,5 @@
+// MEDIA BIMESTRAL
+
 // function main() {
 
 //   let notaBim1, notaBim2, notaBim3, notaBim4, media;
@@ -27,6 +29,8 @@
 //   }
 // }
 // -----------------------------------------------------------------------------------------------------------------
+
+// CONVERSOR
 // function main() {
 //   let reais, cotação, resultado;
 
@@ -40,34 +44,93 @@
 // main();
 // -----------------------------------------------------------------------------------------------------------------
 
-function main() {
-  let reais, cotação, resultado;
-  let moeda;
+// CONVERSOR 2
+// function main() {
+//   let reais, cotação, resultado;
+//   let moeda;
 
-  moeda = prompt("Qual a moeda que você quer converter?");
+//   moeda = prompt("Qual a moeda que você quer converter?");
 
-  if (
-    moeda == "dolar" ||
-    moeda == "dólar" ||
-    moeda == "Dolar" ||
-    moeda == "Dólar"
-  ) {
-    reais = Number(prompt("Quantos reais você quer converter?"));
-    cotação = Number(prompt("Qual a cotação do dólar?"));
-    resultado = reais / cotação;
-    resultado = resultado.toFixed(2);
-    alert("Hoje, você tem " + resultado + " dólares!");
-  } else {
-    if (moeda == "euro" || moeda == "Euro") {
-      reais = Number(prompt("Quantos reais você quer converter?"));
-      cotação = Number(prompt("Qual a cotação do euro?"));
-      resultado = reais / cotação;
-      resultado = resultado.toFixed(2);
-      alert("Hoje, você tem " + resultado + " euros!");
-    } else {
-      alert("Não foi possivel identificar a moeda");
-    }
+//   if (
+//     moeda == "dolar" ||
+//     moeda == "dólar" ||
+//     moeda == "Dolar" ||
+//     moeda == "Dólar"
+//   ) {
+//     reais = Number(prompt("Quantos reais você quer converter?"));
+//     cotação = Number(prompt("Qual a cotação do dólar?"));
+//     resultado = reais / cotação;
+//     resultado = resultado.toFixed(2);
+//     alert("Hoje, você tem " + resultado + " dólares!");
+//   } else {
+//     if (moeda == "euro" || moeda == "Euro") {
+//       reais = Number(prompt("Quantos reais você quer converter?"));
+//       cotação = Number(prompt("Qual a cotação do euro?"));
+//       resultado = reais / cotação;
+//       resultado = resultado.toFixed(2);
+//       alert("Hoje, você tem " + resultado + " euros!");
+//     } else {
+//       alert("Não foi possivel identificar a moeda");
+//     }
+//   }
+// }
+
+// main();
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// // CONVERSOR 3
+// function main() {
+//   let valorConverter, cotacaoMoeda, valorConvertido;
+//   let opcaoMoeda;
+//   let simboloMoeda;
+
+//   opcaoMoeda = prompt(
+//     "Olá, Qual conversão você deseja realizar hoje? 1 - Dólar 2 - Euro"
+//   );
+
+//   if (opcaoMoeda != 1 && opcaoMoeda != 2) {
+//     alert("Opção inválida!");
+//   } else {
+//     valorConverter = prompt("Digite o valor que deseja converter: (em R$)");
+
+//     if (opcaoMoeda == 1) {
+//       cotacaoMoeda = prompt("Digite a cotação do dólar:");
+//       simboloMoeda = "U$";
+//     } else {
+//       cotacaoMoeda = prompt("Digite a cotação do euro:");
+//       simboloMoeda = "€";
+//     }
+//     valorConvertido = (valorConverter / cotacaoMoeda).toFixed(2);
+//     alert(
+//       "O valor convertido para " +
+//         simboloMoeda +
+//         " resultou em " +
+//         simboloMoeda +
+//         valorConvertido
+//     );
+//   }
+// }
+
+// main();
+// -----------------------------------------------------------------------------------------------------------------
+
+// TABUADA
+
+function main(){
+  let tabuada, contador, resultado;
+  let nome;
+
+  nome = prompt("Qual o seu nome");
+  tabuada = prompt(nome + ", qual tabuada você deseja resolver?")
+
+  alert(nome + ", a tabuada do " + tabuada + " é:")
+
+  for(contador = 0; contador <= 10; contador++){
+    resultado = tabuada * contador;
+    alert(tabuada + " x " + contador + " = " + resultado);
   }
+
 }
 
 main();
