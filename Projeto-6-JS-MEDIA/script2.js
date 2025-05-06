1137e8c04ac5ec9f4fc602b885db51ef2092b407
Celso Junior <celso.pljunior@hotmail.com>
2025-05-06 15:24:04 -0300

    Tabuada - concluído

--- Projeto-6-JS-MEDIA/script2.js
+++ Projeto-6-JS-MEDIA/script2.js
@@ -1,28 +1,62 @@
+// function main() {
+//   let tabuada, contador, resultado, resposta;
+//   let nome;
+//   let mensagem = "";
+//   resposta = true;
+//   contador = 0;
+
+//   nome = prompt("Qual o seu nome");
+
+//   while (resposta === true) {
+//     tabuada = Number(prompt(nome + ", qual tabuada você deseja resolver?"));
+//     limite = Number(prompt(nome + ", até qual limite deseja a tabuada?"));
+
+//     alert(nome + ", a tabuada do " + tabuada + " é:");
+//     mensagem = "";
+
+//     for (contador = 0; contador <= 10; contador++) {
+//       resultado = tabuada * contador;
+//       mensagem += tabuada + " x " + contador + " = " + resultado + "\n";
+//     }
+
+//     alert(mensagem);
+
+//     resposta = confirm("Deseja fazer outra tabuada?");
+//   }
+//   alert("Programa finalizado!");
+// }
+
+// main();
+
+
+// ----------------------------------------------------------------------------
+
 function main() {
-  let tabuada, contador, resultado, resposta;
-  let nome;
-  let mensagem = "";
-  resposta = true;
-  contador = 0;
-
-  nome = prompt("Qual o seu nome");
-
-  while (resposta === true) {
-    tabuada = Number(prompt(nome + ", qual tabuada você deseja resolver?"));
-
-    alert(nome + ", a tabuada do " + tabuada + " é:");
-    mensagem = "";
-
-    for (contador = 0; contador <= 10; contador++) {
-      resultado = tabuada * contador;
-      mensagem += tabuada + " x " + contador + " = " + resultado + "\n";
+    let tabuada, contador, resultado, resposta, limite;
+    let nome;
+    let mensagem = "";
+    resposta = true;
+    contador = 0;
+  
+    nome = prompt("Qual o seu nome");
+  
+    while (resposta === true) {
+      tabuada = Number(prompt(nome + ", qual tabuada você deseja resolver?"));
+      limite = Number(prompt(nome + ", até qual limite deseja a tabuada?"));
+  
+      alert(nome + ", a tabuada do " + tabuada + " até o " + limite + " é:");
+      mensagem = "";
+  
+      for (contador = 0; contador <= limite; contador++) {
+        resultado = tabuada * contador;
+        mensagem += tabuada + " x " + contador + " = " + resultado + "\n";
+      }
+  
+      alert(mensagem);
+  
+      resposta = confirm("Deseja fazer outra tabuada?");
     }
-
-    alert(mensagem);
-
-    resposta = confirm("Deseja fazer outra tabuada?");
+    alert("Programa finalizado!");
   }
-  alert("Programa finalizado!")
-}
-
-main();
+  
+  main();
