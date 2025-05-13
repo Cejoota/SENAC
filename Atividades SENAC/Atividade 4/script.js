831bb353e12f6348f43339f2c5cb5b05021a93e4
Cejoota <celso.pljunior@hotmail.com>
2025-05-13 16:51:41 -0300

    Atividade 4 - Concluída

---
+++ Atividades SENAC/Atividade 4/script.js
@@ -0,0 +1,27 @@
+function main(){
+    let num1, num2, contador;
+    let mensagem = "";
+
+    num1 = Number(prompt("Digite o primeiro número: "));
+    num2 = Number(prompt("Digite o segundo número: "));
+
+    if (num1 == num2) {
+        alert("Impossível incrementar ou decrementar!");
+    } else if (num1 < num2) {
+        mensagem += num1 + " - Início\n";
+        for (contador = num1 + 1; contador < num2; contador++) {
+            mensagem += contador + "\n";
+        }
+        mensagem += num2 + " - Fim";
+        alert(mensagem);
+    } else {
+        mensagem += num1 + " - INÍCIO\n";
+        for (contador = num1 - 1; contador > num2; contador--) {
+            mensagem += contador + "\n";
+        }
+        mensagem += num2 + " - FIM";
+        alert(mensagem);
+    }
+}
+
+main();
