6389ba8c96687ee2e393f606e01881ba4d597afb
Celso <celso.pljunior@hotmail.com>
2025-06-06 16:22:19 -0300

    Adicionando Robo-Hover - Atividade concluída

--- Atividades SENAC/Atividade 7 - Animation-Transition/script.js
+++ Atividades SENAC/Atividade 7 - Animation-Transition/script.js
@@ -1,1 +1,12 @@
-function Robo
+function robomover() {
+    
+    const robo = document.getElementById('robotrom');
+    
+    robo.addEventListener('click', () => {
+        robo.classList.remove('robo-js');
+        
+        void robo.offsetWidth;
+        
+        robo.classList.add('robo-js');
+    });
+}
